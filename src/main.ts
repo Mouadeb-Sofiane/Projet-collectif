0882c1c5a4d88d1d5731cbab9ea56b7fb6aae545
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2025-01-09 15:42:41 +0100

    feat(home-suite): Supression des pages inutiles

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,9 @@ import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 // Importer les composants pour les routes
-import Home from './views/Home.vue';
+
 import AboutView from './views/AboutView.vue';
-import CarouselView from './views/CarouselTest.vue';
-import Carousel from './views/CarouselTestAPI.vue';
 import SingleVideoComponent from './views/PageSingleVideo.vue'; 
-import CarouselTest from './views/Carouseltstapi.vue';
-import PocketbaseTest from './views/PocketbaseTest.vue';
 import SingleVideoPocketbase from './views/PageSingleVideoPocketbase.vue';
 import HomePocketbase from './views/HomePocketbase.vue';
 import SinglePocketbase2 from './views/PageSingleVideoPocketbase2.vue';
@@ -20,17 +16,16 @@ import Reportages from './views/Reportages.vue';
 import Emissions from './views/Emissions.vue';
 import APropos from './views/APropos.vue';
 import SingleLive from './views/SingleLive.vue';
-import HomePocketbaseCopy from './views/HomePocketbase_copy.vue';
+
 
 // Définir les routesdv
 const routes = [
   { path: '/', component: HomePocketbase },      
   { path: '/about', component: AboutView },
-  { path: '/carousel', component: CarouselView },
-  { path: '/carouselAPI', component: Carousel },
-  { path : '/carouseltest', component: CarouselTest },
+
+
   { path: '/video/:id', name: 'singleVideo', component: SingleVideoComponent }, 
-  { path: '/pocketbasetest', component: PocketbaseTest },
+
   { path: '/singlepocketbase/:id', name: 'singleVideoPocket', component: SingleVideoPocketbase },
   { path: '/singlepocketbase2/:id', name: 'singleVideoPocket2', component: SinglePocketbase2 },
   { path: '/newsroom', component: NewsRoom },
@@ -38,9 +33,7 @@ const routes = [
   { path: '/reportages', component: Reportages },
   { path: '/emissions', component: Emissions },
   { path: '/apropos', component: APropos },
-  { path: '/homepocket', component:Home },
   { path: '/singlelive', component: SingleLive, name: 'singleLive'},
-  { path: '/homepocketbasecopy', component: HomePocketbaseCopy, name: 'homepocketbase_copy' },
 ];
 
 const router = createRouter({
